9482c2856db830f946b136554e32466d28d63663
emilbesic <43534058+emilbesic@users.noreply.github.com>
2021-04-13 01:33:59 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -55,11 +55,11 @@ vector<vector<int> > initial(int Row, int Col){
     cout << "Cells work with 0 and 1"<< endl;
     for (int i = 0; i < Col; i++) {
         vector<int> v1;
+        cout << "Next row"<< endl;
         for (int j = 0; j < Row; j++) {
             cell = ask_cell();
             v1.push_back(cell);
         }
-        cout << "Next row"<< endl;
         vec.push_back(v1);
     }
     return vec;
@@ -73,10 +73,38 @@ void print_vec(int Row,int Col,vector<vector<int> > board){
     cout << endl;
 }
 }
-int nextState(vector<vector<int>> board, int row, int col){
-    int bor_st = 0;
+vector<vector<int>> cell_neighbour( int row, int col, vector<vector<int>> board){
+    
+    vector<vector<int>> nextboard;
+    
+    for(int x = 1; x < row - 1; x++){
+        for(int y = 1; x < col - 1; y++){
+            int alive = 0;
+            for(int i = -1; i < 2; i++){
+                for(int j = -1; j < 2; j++){
+                    alive += board[x + i][y + j];
+                }
+            } 
+            alive -= board[x][y];
+            
+            if((board[x][y] == 1) && (alive < 2)){
+                nextboard[x][y] = 0;
+            }
+            else if((board[x][y] == 1) && (alive > 3)){
+                nextboard[x][y] = 0;
+            }
+            else if((board[x][y] == 0) && (alive == 3)){
+                 nextboard[x][y] = 1;
+            }
+            else{
+                nextboard[x][y] = board[x][y];
+            }
+        }
+    
+    }
+
 }
-}
+
 int main()
 {
     
@@ -85,6 +113,9 @@ int main()
     vector<vector<int> > board = initial(Row,Col);
     
     print_vec(Row,Col,board);
-
+    cout << "--------------------------------\n";
+    vector<vector<int> > nextboard = cell_neighbour(Row,Col,board);
+    cout << nextboard[0][0];
     return 0;
 }
+
