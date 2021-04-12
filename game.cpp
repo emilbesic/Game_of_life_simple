8a7f44f54d79d8ab3551eff6fbb522df957775dc
emilbesic <43534058+emilbesic@users.noreply.github.com>
2021-04-13 01:32:39 +0300

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,7 +1,14 @@
+
 #include <iostream>
 #include <vector>
+#include <cmath>
+
 using namespace std;
-  
+
+bool is_integer(float k)
+{
+  return std::floor(k) == k;
+}
 
 int askRows(){
     int ROW = 0;
@@ -15,19 +22,30 @@ int askColumns(){
     cin >> COL ;
     return COL;
 }
+int change_one_zero(int num){
+    if(num >= 1){
+        num = 1;
+    }
+    else if(num <= 0){
+        num = 0;
+    }
+    return num;
+}
 vector<vector<int> > initial(int Row, int Col){
     vector<vector<int> > vec;
   
-    // Elements to insert in column
+
     int num = 0;
-  
-    // Inserting elements into vector
+    int cell = 0;
+    cout << "Choose initial states for cells."<< endl;
+    cout << "Cells work with zeroes and ones"<< endl;
+    cout << "Under zero changes to 0 and over 1 changes to 1"<< endl<< "\n";  
     for (int i = 0; i < Col; i++) {
         vector<int> v1;
-  
         for (int j = 0; j < Row; j++) {
             cin >> num;
-            v1.push_back(num);
+            cell = change_one_zero(num);
+            v1.push_back(cell);
         }
         cout << "next row"<< endl;
         // Pushing back above 1D vector
@@ -36,26 +54,25 @@ vector<vector<int> > initial(int Row, int Col){
     }
     return vec;
 }
-void print_vec(int Row,int Col,vector<vector<int> > vec){
-    for (uint8_t i = 0; i < vec.size(); i++) {
-        for (uint8_t j = 0; j < vec[i].size(); j++){
-            cout << vec[i][j] << " ";
+void print_vec(int Row,int Col,vector<vector<int> > board){
+    for (uint8_t i = 0; i < board.size(); i++) {
+        for (uint8_t j = 0; j < board[i].size(); j++){
+            cout << board[i][j] << " ";
         
     }
     cout << endl;
 }
+
 }
 int main()
 {
     
     int Row = askRows();
     int Col = askColumns();
-    vector<vector<int> > vec = initial(Row,Col);
+    vector<vector<int> > board = initial(Row,Col);
     
     // Displaying the 2D vector
-    print_vec(Row,Col,vec);
+    print_vec(Row,Col,board);
 
     return 0;
 }
-
-
